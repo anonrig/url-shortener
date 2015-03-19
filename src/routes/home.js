406e5062d5fb3d45a096c8050c4d0a9866fa8085
Yagiz Nizipli <yagiznizipli@me.com>
2015-03-19 22:49:55 +0200

    Finalize redirection api-side

--- src/routes/home.js
+++ src/routes/home.js
@@ -14,14 +14,29 @@ router.get('/', function(req, res) {
  * Main function called for Url Shortening.
  */
 router.get('/:token', function(req, res) {
-    View.findOne({ 'token': req.params.token }, function(err, current) {
-        if (!err && !current)
+    View.findOne({ 'token': req.params.token }, function(err, view) {
+        if (!err && !view)
             res.render('404');
         else if (err)
             res.render('500');
-        else
-            res.render('view', { 'url': current.url });
-    })
+        else {
+            view.incrementCounter();
+
+            if (req.headers.referrer)
+                view.addReferrer(req.headers.referrer);
+
+            view.save(function(err) {
+                if (err) res.render('500');
+
+                res.render('view', { 'url': view.url });
+                /**
+                 * Alternative solution to HTML5 Refresh.
+                 */
+                // res.redirect(view.url);
+            });
+        }
+
+    });
 });
 
 module.exports = router;
