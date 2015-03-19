983580ee46c099826e67186bdc7e61654b2a0997
Yagiz Nizipli <yagiznizipli@me.com>
2015-03-19 23:37:15 +0200

    Add authentication using cookies

--- src/app/index.js
+++ src/app/index.js
@@ -5,8 +5,50 @@ var express = require('express'),
     morgan = require('morgan'),
     helmet = require('helmet'),
     compression = require('compression'),
-    config = require('urlshortener/config');
+    config = require('urlshortener/config'),
+    passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy,
+    session = require('express-session'),
+    RedisStore = require('connect-redis')(session),
+    redis = require('redis').createClient(),
+    redisStore = new RedisStore({
+        host: config.get('redis:host'),
+        port: config.get('redis:port'),
+        client: redis
+    }),
+    Account = require('urlshortener/components/account/model'),
+    APIError = require('urlshortener/lib/error');
 
+/**
+ * Initialize local strategy for logging in.
+ */
+passport.use(new LocalStrategy({
+        usernameField: 'email',
+        passwordField: 'password'
+    },
+    function(email, password, done) {
+        Account.findOne({ email: email }, function(err, user) {
+            if (err) return done(err);
+            if (user && user.checkPassword(password)) return done(null, user);
+
+            return done(new APIError('user not found', 404), false);
+        });
+    }
+));
+
+/**
+ * Necessary for PassportJS to work.
+ */
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+/**
+ * Necessary for PassportJS to work.
+ */
+passport.deserializeUser(function(id, done) {
+    Account.findById(id, done);
+});
 
 app.enable('trust proxy');
 app.disable('x-powered-by');
@@ -19,6 +61,22 @@ app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan('dev'));
+app.use(session({
+    secret: config.get('session-secret'),
+    proxy: true,
+    cookie: {
+        maxAge: 365 * 24 * 60 * 60
+    },
+    resave: true,
+    saveUninitialized: true,
+    store: redisStore
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+/**
+ * Routing.
+ */
 app.use('/api/v1', require('urlshortener/routes/api'));
 app.use('/', require('urlshortener/routes/home'));
 
