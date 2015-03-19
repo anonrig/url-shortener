f3038234115d8a50add65162c83714204a5d9485
Yagiz Nizipli <yagiznizipli@me.com>
2015-03-19 23:43:04 +0200

    Add create link path

---
+++ src/components/view/controller.js
@@ -0,0 +1,28 @@
+var router = require('express').Router(),
+    APIError = require('urlshortener/lib/error'),
+    auth = require('urlshortener/lib/auth');
+
+var Account = require('./model');
+
+
+/**
+ * Create view path.
+ */
+router.post('/', auth.ensureAuthentication, function(req, res, next) {
+    var view = req.body;
+
+    if (!view.url)
+        next(new APIError('Bad request', 400));
+
+    new Account({
+        url: view.url
+    })
+        .save(function(err) {
+            if (err) return next(err);
+
+            res.status(201).end();
+        });
+});
+
+
+module.exports = router;
